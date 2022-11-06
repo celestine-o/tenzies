f4df9ab7ca709cef4e194e50b5061653efbe2f81
Celestine <macstine@gmail.com>
2022-11-07 00:05:27 +0100

    Added few components
    Added Main and Dice components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Main from './main'
+import Main from './Main'
 
 function App() {
 
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import Dice from "./Dice";
 
 export default function Main() {
 
     return(
-        <div className="main"></div>
+        <div className="main">
+            <h1>Tenzies</h1>
+            <p>
+                Roll untill all dice are the same. Click each die to freeze
+                it at its current value between rolls.
+            </p>
+            <div className="die-container">
+                <Dice />
+            </div>
+            <button>Roll</button>
+        </div>
     )
 }
